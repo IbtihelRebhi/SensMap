f220cc7ccb29f780b164d2e447e0b86b582532b5
IbtihelRebhi <ibtihel.rebhi@enit.utm.tn>
2017-09-26 10:54:53 +0200

    Update Regression_model.R

--- R/Regression_model.R
+++ R/Regression_model.R
@@ -1,7 +1,7 @@
 # prediction with linear models : vect, circul, elliptic and quadratic
 
 predict.scores.lm=function(Y,discretspace,map,
-                           formula="~I(F1*F1)+I(F2*F2)+F1*F2",
+                           formula,
                            pred.na=FALSE){
   ## map is a data.frame with F1 and F2 obtained after DR on the explained data Y
   notespr= nbconsos=matrix(0,nrow(discretspace),ncol(discretspace))
@@ -9,7 +9,7 @@ predict.scores.lm=function(Y,discretspace,map,
   regs=vector("list",ncol(Y))
   nb.NA=vector("list",ncol(Y))
   pos.NA=vector("list",ncol(Y))
-  ## Firts we preform all regressions
+  ## First we preform all regressions
   for(j in 1:ncol(Y)){
     map.reg=cbind.data.frame(Y[,j],map)
     colnames(map.reg)[1]="Conso"
@@ -23,11 +23,11 @@ predict.scores.lm=function(Y,discretspace,map,
       x[x>10]=NA
       pred.conso[,j]=x
       x=as.data.frame(x)
-      nb.NA[[j]] <- apply(x,2,function(a) sum(is.na(a)))# nbre de NA pour chaque conso
-      pos.NA[[j]]=which(is.na(x))# le point qui contient NA pour chaque consom
+      nb.NA[[j]] <- apply(x,2,function(a) sum(is.na(a)))
+      pos.NA[[j]]=which(is.na(x))
       occur.NA <- unlist(pos.NA)
       occur.NA=as.vector(occur.NA)
-      occur.NA=as.data.frame(table(occur.NA))# nbre de NA en chaque point du plan pour tous les consos
+      occur.NA=as.data.frame(table(occur.NA))
 
     }
     else {
@@ -40,5 +40,6 @@ predict.scores.lm=function(Y,discretspace,map,
   return(list(regression=regs,pred.conso=pred.conso,preference=preference,nb.NA=nb.NA,
               pos.NA=pos.NA, occur.NA=occur.NA))
 }
+
 
 
